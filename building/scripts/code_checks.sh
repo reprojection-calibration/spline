1a45c91014069913e7f9b2403ba49ccd2af22961
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-08 14:48:11 +0200

    fix code check includes

--- building/scripts/code_checks.sh
+++ building/scripts/code_checks.sh
@@ -5,4 +5,4 @@ set -eoux pipefail
 find /temporary/building -iname '*.sh' -print0 | xargs --null shellcheck
 
 find /temporary/code \( -iname '*.cpp' -o -iname '*.hpp' -o -iname '*.c' -o -iname '*.h' \) -print0 | xargs --null clang-format --dry-run --Werror
-cppcheck /temporary/code --enable=all --error-exitcode=1 -I /temporary/code/include -I /temporary/code/src  -I /usr/include/apriltag/ --suppress=missingIncludeSystem
+cppcheck /temporary/code --enable=all --error-exitcode=1 -I /temporary/code/include -I /temporary/code/src --suppress=missingIncludeSystem
