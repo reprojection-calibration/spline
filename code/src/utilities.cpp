92db7476c7a6b1f4a10a95d1a0c26995b8f75589
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-08 14:47:28 +0200

    add more descriptive link

--- code/src/utilities.cpp
+++ code/src/utilities.cpp
@@ -3,7 +3,7 @@
 namespace reprojection_calibration::spline {
 
 // Factorial based implementation is not the fastest, but we are dealing with small values (?) so we can afford it for
-// the sake of clarity https://en.wikipedia.org/wiki/Binomial_coefficient
+// the sake of clarity https://en.wikipedia.org/wiki/Binomial_coefficient#Computing_the_value_of_binomial_coefficients
 uint64_t BinomialCoefficient(uint64_t const n, uint64_t const k) {
     return Factorial(n) / (Factorial(k) * Factorial(n - k));
 }
