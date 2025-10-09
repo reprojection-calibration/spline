dab418c3575a5a6192146323d6d45cab3370610f
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-09 10:13:42 +0200

    law of useful return to calculate i

--- code/src/utilities.hpp
+++ code/src/utilities.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <tuple>
 
 namespace reprojection_calibration::spline {
 
 // Calculates what [1] calls "u" - "normalized time elapsed since start of the segment" - see the second paragraph in
-// section 4.2 Matrix Representation
-double NormalizedSegmentTime(uint64_t const t0_ns, uint64_t const t_ns, uint64_t const delta_t_ns);
+// section 4.2 Matrix Representation. In addition to the normalized segment time u we also return the segment index i as
+// this is useful information for error checking later and follows the "law of useful return" principle :)
+std::tuple<double, int> NormalizedSegmentTime(uint64_t const t0_ns, uint64_t const t_ns, uint64_t const delta_t_ns);
 
 // TODO(Jack): How can we also make this calculate the derivatives?
 Eigen::VectorXd TimePolynomial(int const k, double const u);
--- code/src/utilities.test.cpp
+++ code/src/utilities.test.cpp
@@ -9,10 +9,21 @@ using namespace reprojection_calibration::spline;
 // shutter cameras
 
 TEST(Utilities, TestNormalizedSegmentTime) {
-    EXPECT_EQ(NormalizedSegmentTime(100, 100, 5), 0);
-    EXPECT_EQ(NormalizedSegmentTime(100, 115, 5), 0);
-    EXPECT_FLOAT_EQ(NormalizedSegmentTime(100, 116, 5), 0.2);
-    EXPECT_FLOAT_EQ(NormalizedSegmentTime(100, 117, 5), 0.4);
+    // Zero elapsed time edge case
+    auto const [u1, i1]{NormalizedSegmentTime(100, 100, 5)};
+    EXPECT_EQ(u1, 0);
+    EXPECT_EQ(i1, 0);
+
+    // From start of fourth segment (i=3) to 40% through it (0 -> 0.4)
+    auto const [u2, i2]{NormalizedSegmentTime(100, 115, 5)};
+    EXPECT_EQ(u2, 0);
+    EXPECT_EQ(i2, 3);
+    auto const [u3, i3]{NormalizedSegmentTime(100, 116, 5)};
+    EXPECT_FLOAT_EQ(u3, 0.2);
+    EXPECT_EQ(i3, 3);
+    auto const [u4, i4]{NormalizedSegmentTime(100, 117, 5)};
+    EXPECT_FLOAT_EQ(u4, 0.4);
+    EXPECT_EQ(i4, 3);
 }
 
 TEST(Utilities, TestTimePolynomial) {
