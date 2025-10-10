9464ea83977e5d0f3e9b0a00ef58657d917431d6
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-10 11:13:32 +0200

    add lie.test.cpp boilerplate

---
+++ code/src/lie.test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <Eigen/Dense>
+
+namespace reprojection_calibration::spline {}
+
+using namespace reprojection_calibration::spline;
+
+TEST(Lie, TestXXX) { EXPECT_EQ(1, 2); }
