73703b7d0a01e13c6c8de19f37b8bdf4653a72d3
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-10 20:29:34 +0200

    clean up loop

--- code/src/so3_spline.cpp
+++ code/src/so3_spline.cpp
@@ -19,22 +19,18 @@ std::optional<Eigen::Matrix3d> So3Spline::Evaluate(uint64_t const t_ns, Derivati
 
     static MatrixKK const M{CumulativeBlendingMatrix(constants::k)};  // Static means it only evaluates once :)
     // TODO(Jack): Use common generic method one! Pay attention to how we use the constants here though! If we will
-    // alwazs be the same dimension for both position and rotation maybe that simplifies things.
+    // always be the same dimension for both position and rotation maybe that simplifies things.
     VectorK const u{r3Spline::CalculateU(u_i, derivative)};
-
     VectorK const weight{M * u};
 
     // TODO(Jack): Can we replace this all with a std::accumulate call?
     Eigen::Matrix3d result{knots_[i]};
     for (int j{0}; j < (constants::k - 1); ++j) {
-        Eigen::Matrix3d const& p0{knots_[i + j]};
-        Eigen::Matrix3d const& p1{knots_[i + j + 1]};
+        Eigen::Matrix3d const& R0{knots_[i + j]};
+        Eigen::Matrix3d const& R1{knots_[i + j + 1]};
+        Eigen::Vector3d const delta{Log(R0.inverse() * R1)};
 
-        Eigen::Matrix3d const r01{p0.inverse() * p1};
-        Eigen::Vector3d const delta{Log(r01)};
-        Eigen::Vector3d const weighted_delta{weight[j + 1] * delta};
-
-        result *= Exp(weighted_delta);
+        result *= Exp(weight[j + 1] * delta);
     }
 
     return result;
