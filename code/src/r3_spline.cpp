a81453c41d6236e90860ce4f3c9e09dad345a0d6
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-10 11:01:29 +0200

    test CalculateU function

--- code/src/r3_spline.cpp
+++ code/src/r3_spline.cpp
@@ -19,23 +19,24 @@ std::optional<VectorD> r3Spline::Evaluate(uint64_t const t_ns, DerivativeOrder c
 
     MatrixDK const P{Eigen::Map<const MatrixDK>(knots_[i].data(), constants::d, constants::k)};
     static MatrixKK const M{BlendingMatrix(constants::k)};  // Static means it only evaluates once :)
-    VectorK const u{r3Spline::CalculateU(u_i, delta_t_ns_, derivative)};
+    VectorK const u{r3Spline::CalculateU(u_i, derivative)};
 
-    return (P * M * u);
+    return (P * M * u) / std::pow(delta_t_ns_, static_cast<int>(derivative));
 }
 
 // We are constructing the column vectors u that we multiply by C as found at the top of page five in [2] - this
-// construction depends on which derivative we are evaluating the spline at.
+// construction depends on which derivative of u we are evaluating the spline at.
 // TODO(Jack): We also can calculate std::pow(delta_t_ns, derivative_order) in the constructor ahead of time if we
 // find out it causes some problems.
-VectorK r3Spline::CalculateU(double const u_i, uint64_t const delta_t_ns, DerivativeOrder const derivative) {
+VectorK r3Spline::CalculateU(double const u_i, DerivativeOrder const derivative) {
+    assert(0 <= u_i and u_i < 1);
+
     static MatrixKK const polynomial_coefficients{
         PolynomialCoefficients(constants::k)};  // Static means it only evaluates once :)
 
     int const derivative_order{static_cast<int>(derivative)};
     VectorK const u{polynomial_coefficients.row(derivative_order).transpose().array() *
-                    TimePolynomial(constants::k, u_i, derivative_order).array() /
-                    std::pow(delta_t_ns, derivative_order)};
+                    TimePolynomial(constants::k, u_i, derivative_order).array()};
 
     return u;
 }
--- code/src/r3_spline.hpp
+++ code/src/r3_spline.hpp
@@ -19,7 +19,7 @@ class r3Spline {
                                     DerivativeOrder const derivative = DerivativeOrder::Null) const;
 
     // TODO(Jack): Can we use this same method also for the rotation spline?
-    static VectorK CalculateU(double const u_i, uint64_t const delta_t_ns,
+    static VectorK CalculateU(double const u_i,
                               DerivativeOrder const derivative = DerivativeOrder::Null);
 
     // TODO(Jack): Let us consider what benefit we would get from making this private at some later point
