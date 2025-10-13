73f1f7fbbe6c54d14208e530f9494fe3edc5c371
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-13 09:42:49 +0200

    add code coverage github action

--- building/scripts/code_coverage.sh
+++ building/scripts/code_coverage.sh
@@ -2,7 +2,19 @@
 
 set -eoux pipefail
 
-lcov --directory /buildroot/build --capture --output-file coverage.info --rc geninfo_auto_base=1 --ignore-errors mismatch,mismatch
+apt-get update
+apt-get install --no-install-recommends --yes \
+  bc
 
+# Adopted from https://github.com/svnscha/cpp-coverage-example
+lcov --directory /buildroot/build --capture --output-file coverage.info --rc geninfo_auto_base=1 --ignore-errors mismatch,mismatch
 lcov --remove coverage.info '/usr/*' '*.test.cpp' '*gtest*' --output-file /buildroot/coverage.filtered.info
 genhtml coverage.filtered.info --output-directory /buildroot/coverage-report
+
+coverage_percent=$(lcov --summary /buildroot/coverage.filtered.info | grep "lines" | awk '{ print $2 }' | sed 's/%//')
+if (( $(echo "${coverage_percent} < 100" | bc -l) )); then
+  echo "Coverage only ${coverage_percent}, please inspect report to get 100% coverage."
+  exit 1
+else
+  echo "Full coverage achieved."
+fi
