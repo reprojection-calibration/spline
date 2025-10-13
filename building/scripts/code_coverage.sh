914129870df6e5bf5d6672544e1532bedd353f58
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-13 11:02:50 +0200

    exclude false positive lines

--- building/scripts/code_coverage.sh
+++ building/scripts/code_coverage.sh
@@ -4,5 +4,5 @@ set -eoux pipefail
 
 # Adopted from https://github.com/svnscha/cpp-coverage-example
 lcov --directory /buildroot/build --capture --output-file coverage.info --rc geninfo_auto_base=1 --ignore-errors mismatch,mismatch
-lcov --remove coverage.info '/usr/*' '*.test.cpp' '*gtest*' --output-file /buildroot/coverage.filtered.info
-genhtml coverage.filtered.info --output-directory /buildroot/coverage-report
+lcov --remove coverage.info '/usr/*' '*.test.cpp' '*gtest*' --output-file coverage.filtered.info
+genhtml coverage.filtered.info --output-directory coverage-report
